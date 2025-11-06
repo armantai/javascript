d33547d13738b5ddcec5ed731a6bfcec173df452
armantai <taimunna722@gmail.com>
2025-11-06 11:42:21 +0530

    Add files via upload

---
+++ javascript/day 1/index.js
@@ -0,0 +1,40 @@
+// let a = 3
+// let b = 20
+// let i
+// let n=10
+
+// console.log("This is the a or b sum is =",a+b)
+
+// for(i=1; i<=n; i++){
+//     console.log( a,"X" ,i ,"=",a*i)
+// }
+
+
+
+// let age=prompt("What is your age")
+
+// if(age>=18){
+//     console.log("You can  drive")
+// }
+// else{
+//     console.log("you can not drive")
+// }
+
+
+let choice=prompt("Enter your choice:")
+
+switch(choice){
+    case 1:
+        console.log("First Nuber")
+        break;
+        case 2:
+        console.log("secound Nuber")
+        break;
+        case 3:
+        console.log("third Nuber")
+        break;
+    
+    
+    default:
+    break;
+}
